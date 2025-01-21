fe88d13514211f004e58bad824f0d0648f8faadd
junilab <99321642+junilab@users.noreply.github.com>
2025-01-21 16:16:42 +0900

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,10 @@
+//% color="#AA278D" weight=100 icon="\uf0c3"
+namespace main {
+    /**
+     * Adds two numbers
+     */
+    //% block
+    export function addNumbers(a: number, b: number): number {
+        return a + b;
+    }
+}

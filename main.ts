443b1a4b5a500b6ee81cf923b7176b59efb70277
junilab <99321642+junilab@users.noreply.github.com>
2025-01-21 17:31:49 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,35 @@
 //% color="#AA278D" weight=100 icon="\uf0c3"
 namespace main {
+    
     /**
-     * Adds two numbers
+     * Adds two numbers and returns the result
      */
     //% block
     export function addNumbers(a: number, b: number): number {
         return a + b;
     }
-     /**
-     * minus two numbers
+
+    /**
+     * Subtracts the second number from the first
      */
     //% block
-    export function minusNumbers(a: number, b: number): number {
+    export function subtractNumbers(a: number, b: number): number {
         return a - b;
+    }
+
+    /**
+     * Turns on an LED at a specific pin
+     */
+    //% block="turn on LED at pin %pin"
+    export function turnOnLED(pin: DigitalPin): void {
+        pins.digitalWritePin(pin, 1);
+    }
+
+    /**
+     * Turns off an LED at a specific pin
+     */
+    //% block="turn off LED at pin %pin"
+    export function turnOffLED(pin: DigitalPin): void {
+        pins.digitalWritePin(pin, 0);
     }
 }

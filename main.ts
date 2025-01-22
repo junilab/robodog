d25f533552289d03163c768d48a72f27687a1119
junilab <99321642+junilab@users.noreply.github.com>
2025-01-22 14:12:49 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,39 @@
 //% color="#AA278D" weight=100 icon="\uf0c3"
 namespace main {
+    let isInit = 0;
+    let buffer: Buffer = null;
     export let counter = 0; // 네임스페이스 내부 전역 변수
 
-    loops.everyInterval(1000, function () {
-        counter += 1;
-        basic.showNumber(counter);
-    });
+    function checksum(buffer: Buffer): number {
+        let sum = 0;
+        for (let i = 6; i < buffer.length; i++) {
+            sum += buffer[i];
+        }
+        return sum & 0xFF;
+    }
     
-    //% block="increase counter by $value"
-    export function increaseCounter(value: number): void {
-        counter += value; // 전역 변수 사용
+    loops.everyInterval(1000, function () {
+        if(isInit == 0){
+            serial.redirect(SerialPin.P0, SerialPin.P1, BaudRate.BaudRate115200);
+            buffer = pins.createBuffer(48);
+            buffer.setNumber(NumberFormat.UInt8LE, 0, 0x26);
+            buffer.setNumber(NumberFormat.UInt8LE, 1, 0xA8);
+            buffer.setNumber(NumberFormat.UInt8LE, 2, 0x14);
+            buffer.setNumber(NumberFormat.UInt8LE, 3, 0x81);
+            buffer.setNumber(NumberFormat.UInt8LE, 4, 48);
+            isInit = 1;
+        }
+        buffer[5] = checksum(buffer);
+        serial.writeBuffer(buffer);
+        basic.showNumber(buffer[5]);
+    });
+
+    
+    
+    //% block="gesture by $value"
+    export function gesture(value: number): void {
+            buffer[15] = 0x04;
+            buffer[16] = value;
     }
     
     /**

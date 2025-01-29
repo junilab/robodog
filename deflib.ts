6885542514fc9b0ff902301b5d290a04e54e951c
junilab <99321642+junilab@users.noreply.github.com>
2025-01-29 18:09:55 +0900

    Update deflib.ts

--- deflib.ts
+++ deflib.ts
@@ -3,7 +3,7 @@ namespace Deflib {
     export enum leg {
         LEFT_FRONT = 0,
         LEFT_BACK = 1,
-        RIGHT_BACK 2,
+        RIGHT_BACK = 2,
         RIGHT_FRONT = 3,
         FRONT_LEG = 4,
         BACK_LEG = 5,
